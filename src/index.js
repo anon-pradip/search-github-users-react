1da36d9136f85b2e13eca90af1b97779741c1810
anon-pradip <pradipsubedi360@gmail.com>
2022-11-21 10:48:29 +0545

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -12,18 +12,16 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Auth0Provider
-      domain="dev-zgrk2zzimmxudv7p.us.auth0.com"
-      clientId="AjO28UJy1LrvtdQUx2vXM37aoI4uaSzL"
-      redirectUri={window.location.origin}
-      cacheLocation='localstorage'
-    >
-      <GithubProvider>
-        <App />
-      </GithubProvider>
-    </Auth0Provider>
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-zgrk2zzimmxudv7p.us.auth0.com"
+    clientId="AjO28UJy1LrvtdQUx2vXM37aoI4uaSzL"
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <GithubProvider>
+      <App />
+    </GithubProvider>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
